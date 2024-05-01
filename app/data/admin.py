36b994d4fb32efe1e8a0922865cd416093c46392
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2024-05-01 22:45:42 +0300

    [ Add ] admin models

--- app/data/admin.py
+++ app/data/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# Пользователь
+# -------------------------------------------------------------------------
+@admin.register(AppUser)
+class AppUserAdminModel(admin.ModelAdmin):
+    list_display = ['username', 'is_active', 'is_staff', 'is_superuser']
+
+
+# Клуб
+# -------------------------------------------------------------------------
+@admin.register(Club)
+class ClubAdminModel(admin.ModelAdmin):
+    list_display = ['title', 'adress']
+
+
+# Компьютер
+# -------------------------------------------------------------------------
+@admin.register(PC)
+class PCAdminModel(admin.ModelAdmin):
+    list_display = ['num', 'club', 'pc_level']
+    list_filter = ['club', 'pc_level']
+
+
+# Уровень компьютера
+# -------------------------------------------------------------------------
+@admin.register(PCLevel)
+class PCLevelAdminModel(admin.ModelAdmin):
+    pass
